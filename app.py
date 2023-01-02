8d3dd1aa97dbc73d23f6ce5694a30cab8422067f
osawa-koki <github@osawa.dev>
2023-01-03 05:48:48 +0900

    コマンドライン引数で指定する部分をコード内に。

--- app.py
+++ app.py
@@ -7,8 +7,17 @@ app = FastAPI()
 # Serve static files from the "static" directory
 app.mount("/", StaticFiles(directory="static"), name="static")
 
+# Set the host and port values
+host = "0.0.0.0"
+port = 80
+
 # Define a route to handle the /api URL
 @app.get("/api")
 def api():
   # Return a JSON response with the message "Hello, world!"
   return JSONResponse({"message": "Hello, world!"})
+
+# Run the FastAPI app
+if __name__ == "__main__":
+  import uvicorn
+  uvicorn.run(app, host=host, port=port, log_level="info")
