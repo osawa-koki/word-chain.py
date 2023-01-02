b3c8e5c1348aeeea44827c6d133a5e50ef99a31f
osawa-koki <github@osawa.dev>
2023-01-03 08:34:41 +0900

    共通部分の処理。

--- client/pages/reference.tsx
+++ client/pages/reference.tsx
@@ -1,14 +1,17 @@
 import PageBlock from "../components/pages";
+import Layout from '../components/Layout';
 
 export default function HelloWorld() {
   return (
-    <main>
-      <div id="Ref">
-        <h1>Here,  About page.</h1>
-        <p>このページは<a href="https://chat.openai.com/chat">ChatGPT</a>にアドバイスを受けて作成しました。<br />またCopilotの協力も当然に受けています。</p>
-        <hr />
-        <PageBlock />
-      </div>
-    </main>
+    <Layout>
+      <main>
+        <div id="Ref">
+          <h1>Here,  About page.</h1>
+          <p>このページは<a href="https://chat.openai.com/chat">ChatGPT</a>にアドバイスを受けて作成しました。<br />またCopilotの協力も当然に受けています。</p>
+          <hr />
+          <PageBlock />
+        </div>
+      </main>
+    </Layout>
   );
 };
