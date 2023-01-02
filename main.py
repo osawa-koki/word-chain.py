a5d2fff22e6954bd8074d454a3a759739fb43882
osawa-koki <github@osawa.dev>
2023-01-03 07:18:05 +0900

    CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 import pulp
